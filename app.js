96e2e4d7b26ea6ae469733e098855bf52e7be3bf
Hristo <hristostf@gmail.com>
2020-08-31 17:31:30 +0300

    remove yarn lock file

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	return res.status(200).send('test message');
+	return res.status(200).send('test message 3000');
 });
 
 app.listen(5000);
