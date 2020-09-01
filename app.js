ef875932066682d90dbfff089dd9784d69b0e90a
Hristo <hristostf@gmail.com>
2020-09-01 11:24:41 +0300

    Allow cross origin access

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.set('json spaces', 40);
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	next();
+});
+
 app.use('/api/v1', apiRouts);
 app.use((req, res) => {
 	res.status(200).send('welcome to my api');
